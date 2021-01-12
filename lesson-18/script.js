f4381b9fcc1913964f957feb9f525ad62bb4cbff
Kate <katerina.savras@gmail.com>
2021-01-12 09:42:21 +0300

    homework part2

--- lesson-18/script.js
+++ lesson-18/script.js
@@ -1,8 +1,6 @@
 let age = document.querySelector("#age");
-
 let name = document.querySelector("#name");
 let birthDate = document.querySelector("#birthDate");
-
 let education = document.getElementById("education");
 let educationOption = [...education.options];
 let gender = [...document.getElementsByName("gender")];
@@ -17,38 +15,39 @@ let close = document.querySelector("#close");
 let modal = document.querySelector(".modal");
 
 
-
 btnSubmit.addEventListener("click", () => {
+    name.style.outline = "none";
     if (name.value === "") {
         name.style.border = "2px solid red";
 
     } else {
-        nameValue.innerHTML = `Имя: ${name.value}`;
-        ageValue.innerHTML = `Возраст: ${age.value} года`;
+        name.style.border = "2px solid green";
+        nameValue.innerHTML = `Name: ${name.value}`;
+        ageValue.innerHTML = `Age: ${age.value} years old`;
         for (let y = 0; y < educationOption.length; y++) {
             if (educationOption[0].selected) {
-                educationValue.innerHTML = `Образование: ${educationOption[0].value}`;
+                educationValue.innerHTML = `Education: ${educationOption[0].value}`;
                 break;
             } else if (educationOption[1].selected) {
-                educationValue.innerHTML = `Образование: ${educationOption[1].value}`;
+                educationValue.innerHTML = `Education: ${educationOption[1].value}`;
                 break;
             } else if (educationOption[2].selected) {
-                educationValue.innerHTML = `Образование: ${educationOption[2].value}`;
+                educationValue.innerHTML = `Education: ${educationOption[2].value}`;
                 break;
             }
         }
         for (let i = 0; i < gender.length; i++) {
             if (gender[0].checked) {
-                genderValue.innerHTML = `Пол: ${gender[0].value}`;
+                genderValue.innerHTML = `Gender: ${gender[0].value}`;
                 console.log(gender[0]);
                 break;
             } else if (gender[1].checked) {
-                genderValue.innerHTML = `Пол: ${gender[1].value}`;
+                genderValue.innerHTML = `Gender: ${gender[1].value}`;
                 console.log(gender[1]);
                 break;
             }
         }
-        dataValue.innerHTML = `Дата рождения: ${birthDate.value}`;
+        dataValue.innerHTML = `Birth date: ${birthDate.value}`;
         modal.style.display = "block"
     }
 
@@ -59,14 +58,14 @@ close.addEventListener("click", () => {
 });
 
 function validateAge() {
-    console.log(age.value)
+
     age.style.outline = "none";
     if (!/^[0-9]+$/.test(age.value)) {
         age.style.border = "2px solid red";
-        console.log("red")
+
     } else {
         age.style.border = "2px solid green";
-        console.log("grean")
+
     }
 };
 
