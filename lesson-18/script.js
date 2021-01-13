448820af1dd217df27818bf0b1de35729571a2a9
Kate <katerina.savras@gmail.com>
2021-01-13 14:11:45 +0300

    homework part3

--- lesson-18/script.js
+++ lesson-18/script.js
@@ -2,10 +2,7 @@ let age = document.querySelector("#age");
 let name = document.querySelector("#name");
 let birthDate = document.querySelector("#birthDate");
 let education = document.getElementById("education");
-let educationOption = [...education.options];
-let gender = [...document.getElementsByName("gender")];
 let btnSubmit = document.querySelector(".btnSubmit");
-
 let ageValue = document.querySelector("#ageValue");
 let nameValue = document.querySelector("#nameValue");
 let genderValue = document.querySelector("#genderValue");
@@ -13,61 +10,60 @@ let educationValue = document.querySelector("#educationValue");
 let dataValue = document.querySelector("#dataValue");
 let close = document.querySelector("#close");
 let modal = document.querySelector(".modal");
+let form = document.querySelector(".form");
+let error = document.querySelector(".error");
 
 
 btnSubmit.addEventListener("click", () => {
+
+    if(!validateName() && !validateAge()){
+        nameValue.innerHTML = `Name: ${name.value}`;
+        ageValue.innerHTML = `Age: ${age.value} years old`;
+
+        let educationOption = education.options[education.selectedIndex].text;
+        educationValue.innerHTML = `Education: ${educationOption}`;
+
+        let gender = document.querySelector('input[name = "gender"]:checked').value;
+        genderValue.innerHTML = `Gender: ${gender}`;
+
+        dataValue.innerHTML = `Birth date: ${birthDate.value}`;
+
+        modal.style.display = "block"
+}
+});
+
+let validateName = () => {
     name.style.outline = "none";
     if (name.value === "") {
         name.style.border = "2px solid red";
-
+        error.style.display = "block";
+        return true
     } else {
-        name.style.border = "2px solid green";
-        nameValue.innerHTML = `Name: ${name.value}`;
-        ageValue.innerHTML = `Age: ${age.value} years old`;
-        for (let y = 0; y < educationOption.length; y++) {
-            if (educationOption[0].selected) {
-                educationValue.innerHTML = `Education: ${educationOption[0].value}`;
-                break;
-            } else if (educationOption[1].selected) {
-                educationValue.innerHTML = `Education: ${educationOption[1].value}`;
-                break;
-            } else if (educationOption[2].selected) {
-                educationValue.innerHTML = `Education: ${educationOption[2].value}`;
-                break;
-            }
-        }
-        for (let i = 0; i < gender.length; i++) {
-            if (gender[0].checked) {
-                genderValue.innerHTML = `Gender: ${gender[0].value}`;
-                console.log(gender[0]);
-                break;
-            } else if (gender[1].checked) {
-                genderValue.innerHTML = `Gender: ${gender[1].value}`;
-                console.log(gender[1]);
-                break;
-            }
-        }
-        dataValue.innerHTML = `Birth date: ${birthDate.value}`;
-        modal.style.display = "block"
+        return  false
     }
+}
 
-});
 
-close.addEventListener("click", () => {
-    modal.style.display = "none";
-});
-
-function validateAge() {
-
+let validateAge = () => {
     age.style.outline = "none";
     if (!/^[0-9]+$/.test(age.value)) {
         age.style.border = "2px solid red";
-
+        return true
     } else {
         age.style.border = "2px solid green";
-
+        return false
     }
-};
+}
+
+
+close.addEventListener("click", () => {
+    modal.style.display = "none";
+    name.style.border = "1px solid black";
+    error.style.display = "none";
+    age.style.border = "1px solid black";
+    form.reset();
+
+});
 
 
 
