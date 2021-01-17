4a1a38219dac0a1e50fd64ff542995be64a6672d
Kate <katerina.savras@gmail.com>
2021-01-17 21:05:02 +0300

    commit part1

--- lesson-19/script.js
+++ lesson-19/script.js
@@ -1,70 +1,90 @@
 let btnSubmit = document.querySelector('.btnSubmit')
 let card = document.querySelector('.card');
-let cardWrapper = document.querySelector('.card__wrapper');
-let i = 0;
+let form = document.querySelector('form');
+let inputTitle = document.querySelector('#title');
+let inputDesc = document.querySelector('#description');
+let modal = document.querySelector(".modal");
+let btnOk = document.querySelector(".btnOk");
+
+let curTitle;
+let curDesc;
+let arr = [];
 
 btnSubmit.addEventListener("click", () => {
-    let form = document.querySelector('form');
-    let titleValue;
-    let descriptionValue;
-    let btnDelete;
-    let divGroup;
-    let btnEdit;
-    let arr = [];
-
-    let inputs = document.querySelectorAll('input');
-    inputs.forEach((element) => {
-        arr.push({title: element.value, description: element.value});
-
-    });
-    divGroup = document.createElement('div');
-    card.prepend(divGroup);
-    divGroup.classList.add(i);
-
-    btnDelete = document.createElement('button');
-    btnDelete.classList.add("text__element")
-    btnDelete.innerHTML = "Delete";
-    btnDelete.addEventListener("click", ()=>{
-        divGroup.remove();
-    })
-
-
-    btnEdit = document.createElement('button');
-    btnEdit.classList.add("text__element")
-    btnEdit.innerHTML = "Edit";
-
-
-    descriptionValue = document.createElement('p');
-    descriptionValue.classList.add("text__element")
-    descriptionValue.innerHTML = `${arr[1].description}`;
-
-
-
-    titleValue = document.createElement('p');
-    titleValue.classList.add("text__element")
-    titleValue.innerHTML = `${arr[0].title}`;
-
-
-    divGroup.prepend(btnDelete);
-
-    divGroup.prepend(btnEdit);
-    divGroup.prepend(descriptionValue);
-    divGroup.prepend(titleValue);
-
-
-    i++;
-    console.log(i)
-    form.reset();
+    arr.push({title: inputTitle.value, description: inputDesc.value});
+    recreateCards();
 });
 
-// divGroup.addEventListener("click", (event) => {
-//     if (event.target.textContent === "Delete") {
-//
-//         .remove();
-//
-//     }
-//     if (event.target.textContent === "Edit") {
-//         console.log("no")
-//     }
-// })
+let recreateCards = () => {
+    card.innerHTML = "";
+    for (let element of arr) {
+        let divGroup = document.createElement('div');
+        card.prepend(divGroup);
 
+        let btnDelete = document.createElement('button');
+        btnDelete.classList.add("text__element")
+        btnDelete.innerHTML = "Delete";
+        divGroup.prepend(btnDelete);
+
+
+        let btnEdit = document.createElement('button');
+        btnEdit.classList.add("text__element")
+        btnEdit.innerHTML = "Edit";
+        divGroup.prepend(btnEdit);
+
+        let descriptionValue = document.createElement('p');
+        descriptionValue.classList.add("text__element")
+        descriptionValue.innerHTML = `${element.description}`;
+        divGroup.prepend(descriptionValue);
+
+        let titleValue = document.createElement('p');
+        titleValue.classList.add("text__element")
+        titleValue.innerHTML = `${element.title}`;
+        divGroup.prepend(titleValue);
+        form.reset();
+    }
+}
+
+
+card.addEventListener("click", (event) => {
+    if (event.target.textContent === "Delete") {
+        let curParent = event.target.parentElement;
+        let curTitle = curParent.childNodes[0].innerText;
+        let curDesc = curParent.childNodes[1].innerText;
+        deleteElement(curTitle, curDesc)
+    }
+    if (event.target.textContent === "Edit") {
+        let curParent = event.target.parentElement;
+        curTitle = curParent.childNodes[0].innerText;
+        curDesc = curParent.childNodes[1].innerText;
+        modal.style.display = "block";
+    }
+});
+
+const deleteElement = (curTitle, curDesc) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].title === curTitle && arr[i].description === curDesc) {
+            arr.splice(i, 1);
+            break;
+        }
+    }
+    recreateCards();
+};
+
+const editElement = (curTitle, curDesc, newTitle, newDesc) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].title === curTitle && arr[i].description === curDesc) {
+            arr[i].title = newTitle;
+            arr[i].description = newDesc;
+            break;
+        }
+    }
+    recreateCards();
+};
+
+btnOk.addEventListener("click", () => {
+    let newTitle = document.querySelector("#titleNew").value;
+    let newDesc = document.querySelector("#descNew").value;
+    modal.style.display = "none";
+    editElement(curTitle, curDesc, newTitle, newDesc);
+});
