bf74dcbe6597076f58d063ddf22ce5b3930fed90
Kate <katerina.savras@gmail.com>
2021-01-25 18:27:26 +0300

    add all function

--- lesson-21/script.js
+++ lesson-21/script.js
@@ -10,6 +10,7 @@ let tbody = document.querySelector('.tab_body');
 let numberEmploy = document.querySelector('#numberEmploy');
 let sumSalary = document.querySelector('#sumSalary');
 let sortSalary = document.querySelector('#sortSalary');
+let sortBirthday = document.querySelector('#sortBirthday');
 let sortDate = document.querySelector('#sortDate');
 
 
@@ -18,12 +19,12 @@ let counter = 0;
 btnAdd.addEventListener("click", () => {
     arr.push({name: name.value, birthday: birthday.value, day: day.value, salary: salary.value, id: counter});
     console.log(arr);
-    createTable();
+    createTable(arr);
     form.reset();
 })
-let createTable = () => {
+let createTable = (array) => {
     tbody.innerHTML = " ";
-    arr.forEach((element) => {
+    array.forEach((element) => {
         tbody.innerHTML += `
      <tr class="string" id=${element.id}>
      <td><input type="checkbox" name="worker" class="worker"></td>
@@ -34,20 +35,20 @@ let createTable = () => {
      </tr>`;
     });
     numberEmploy.innerHTML = `${arr.length}`;
-
+    counter++;
 }
 
-btnDelete.addEventListener("click", (event) => {
+btnDelete.addEventListener("click", () => {
     let workers = [...document.querySelectorAll('input[name = "worker"]:checked')];
-    console.log(workers);
     workers.forEach((item) => {
         let trParent = item.parentElement.parentElement;
+        console.log(trParent)
         trParent.remove();
         removeItemById(trParent.id)
     })
-    console.log(arr);
     numberEmploy.innerHTML = `${arr.length}`;
 })
+
 const removeItemById = (id) => {
     const index = arr.findIndex(
         (elem) => elem.id === (Number(id))
@@ -57,26 +58,28 @@ const removeItemById = (id) => {
 
 btnCount.addEventListener("click", () => {
     let sum = 0;
-    let sal;
-    for (let i = 0; i < arr.length; i++) {
-        sal = arr[i].salary;
-        sum += Number(sal);
+    let employers = [...document.querySelectorAll('input[name = "worker"]:checked')];
+    employers.forEach((item) => {
+        let checkSalary = item.parentElement.parentElement.lastElementChild.textContent;
+        sum += Number(checkSalary);
         console.log(sum);
-    }
+    })
     sumSalary.innerHTML = `${sum}`;
 })
 
 sortSalary.addEventListener("click", () => {
-    let sortArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        sortArr.push(arr[i].salary);
-        sortArr.sort(function compareNumbers(a, b) {
-            return a - b;
-        });
-        console.log(sortArr);
-    }
+    let sortArrSalary = arr.sort((prev, next) => prev.salary - next.salary);
+    createTable(sortArrSalary);
 })
 
+sortBirthday.addEventListener("click", () => {
+    let sortArrBirthday = arr.sort((prev, next) => prev.birthday - next.birthday);
+    createTable(sortArrBirthday);
+})
 
 sortDate.addEventListener("click", () => {
+    let sortArrDate = arr.sort((a, b) => new Date(a.day) - new Date(b.day))
+    createTable(sortArrDate);
+
 })
+
